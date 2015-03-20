24f4e62387e6f1849917dcae0e0f24ef66f2f603
KangKung Yun <kangkungyun@KangKungui-MacBook-Air.local>
2015-03-21 00:42:04 +0900

    Initial commit

---
+++ app/src/main/java/com/google/devrel/samples/ttt/ClientCredentials.java
@@ -0,0 +1,5 @@
+package com.google.devrel.samples.ttt;
+
+public class ClientCredentials {
+  public static final String AUDIENCE = "server:client_id:555772538947-bfb3omdo08vemo6g8ke3d51qagoq13d1.apps.googleusercontent.com";
+}
